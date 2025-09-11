66b3ccffeb80d73bacf7e801f5775c9986c72d57
77 <2319927549@qq.com>
2025-09-11 20:01:27 +0800

    P5705

--- @Luogu/P5705.c
+++ @Luogu/P5705.c
@@ -3,14 +3,14 @@
 int main()
 {
     float init;
-    scanf("%d", &init);
+    scanf("%f", &init);
     int int_init = (int)(init * 10);
     int a = int_init % 10;
-    int b = int_init / 10 % 100;
-    int c = int_init / 100 % 1000;
+    int b = int_init / 10 % 10;
+    int c = int_init / 100 % 10;
     int d = int_init / 1000;
     int sum = a * 1000 + b * 100 + c * 10 + d;
-    float float_sum = sum/1000;
+    float float_sum = sum/1000.0;
     printf("%.3f",float_sum);
     return 0;
 }
