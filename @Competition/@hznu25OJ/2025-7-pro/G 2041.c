a622f8c08d93f3c169e1d77f84195c3130048d39
77 <2319927549@qq.com>
2025-10-25 19:02:27 +0800

    10.25

--- @Competition/@hznu25OJ/2025-7-pro/G 2041.c
+++ @Competition/@hznu25OJ/2025-7-pro/G 2041.c
@@ -15,61 +15,58 @@ int main()
             return 0;
         }
 
-        int yuyuedu_max = 0;
+        struct Jieju
+        {
+            int yuyuedu;
+            int time;
+        } jieju[10000];
+        int total_n_jieju = 0;
+
         for (int n_game = 0; n_game < N; n_game++)
         {
-            // n_jieju种结局
             int n_jieju;
             scanf("%d", &n_jieju);
-            // 每一个结局
-            struct Jieju
+
+            int yuyuedu_temp[1000];
+            for (int i = 0; i < n_jieju; i++)
             {
-                int yuyuedu;
-                int time;
-            } jieju[n_jieju];
+                scanf("%d", &yuyuedu_temp[i]);
+            }
 
             for (int i = 0; i < n_jieju; i++)
             {
-                scanf("%d", &jieju[i].yuyuedu);
-            }
-            for (int i = 0; i < n_jieju; i++)
-            {
-                scanf("%d", &jieju[i].time);
-            }
-
-            // 01背包!!!
-            int dp[n_jieju + 1][T + 1];
-            for (int i = 0; i <= n_jieju; i++)
-            {
-                for (int j = 0; j <= T; j++)
-                {
-                    dp[i][j] = 0;
-                }
-            }
-            for (int i = 1; i <= n_jieju; i++)
-            {
-                for (int j = 1; j <= T; j++)
-                {
-                    if (jieju[i - 1].time > j)
-                    {
-                        dp[i][j] = dp[i - 1][j];
-                    }
-                    else
-                    {
-                        dp[i][j] = max(dp[i - 1][j], jieju[i - 1].yuyuedu + dp[i - 1][j - jieju[i - 1].time]);
-                    }
-                }
-            }
-
-            if (dp[n_jieju][T] > yuyuedu_max)
-            {
-                yuyuedu_max = dp[n_jieju][T];
+                int time_temp;
+                scanf("%d", &time_temp);
+                jieju[total_n_jieju].yuyuedu = yuyuedu_temp[i];
+                jieju[total_n_jieju].time = time_temp;
+                total_n_jieju++;
             }
         }
-        printf("%d\n", yuyuedu_max);
+
+        // 01背包, 创建dp表
+        int dp[total_n_jieju + 1][T + 1];
+        for (int j = 0; j <= T; j++)
+        {
+            dp[0][j] = 0;
+        }
+
+        for (int i = 1; i <= total_n_jieju; i++)
+        {
+            for (int j = 0; j <= T; j++)
+            {
+                if (j >= jieju[i - 1].time)
+                {
+                    dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - jieju[i - 1].time] + jieju[i - 1].yuyuedu);
+                }
+                else
+                {
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
+        }
+
+        printf("%d\n", dp[total_n_jieju][T]);
     }
 
     return 0;
 }
-
-// tmd重新写吧
